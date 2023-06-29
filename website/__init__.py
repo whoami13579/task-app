b08bbf874576f259caaa06f662f08800a618dd7c
Richard_Chu <liangforcoding@gmail.com>
2023-06-29 19:08:20 +0800

    add database

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = 'iwonttellyou'
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    db.init_app(app)
+
+    from .models import User
+    with app.app_context():
+        db.create_all()
 
     from .views import views
     app.register_blueprint(views, url_prefix="/")
