6719db80bbf6d09802b9126c5f5c3f2b2abee20b
Richard_Chu <liangforcoding@gmail.com>
2023-07-07 00:42:19 +0800

    log out

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, login_required, logout_user
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
@@ -59,3 +59,9 @@ def sign_up():
             return redirect(url_for("views.home"))
 
     return render_template("signup.html", user=current_user)
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("views.home"))
